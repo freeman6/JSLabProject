834724ef8f1a1fc63acf54b0a1a8ff6d9af21cab
freeman6 <freeman6@gmail.com>
2021-10-23 13:03:13 +0800

    WebAPI 加入 CORS 允許跨域存取。

--- WebAPI/Controllers/ProductController.cs
+++ WebAPI/Controllers/ProductController.cs
@@ -6,9 +6,11 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
 using WebAPI.Models;
+using System.Web.Http.Cors;
 
 namespace WebAPI.Controllers
 {
+    [EnableCors(origins:"*", headers:"*", methods: "*")]
     public class ProductController : ApiController
     {
         public List<Product> products = ProductHelper.Products;
